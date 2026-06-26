89d648b0bc9d06b3a7fea18bef28ecf6ce54a45e
Shiv Sharan Tripathi <tripathishivsharan898@gmail.com>
2026-06-26 23:21:23 +0530

    remove word

---
+++ removeword.py
@@ -0,0 +1,7 @@
+ch=input("Enter a string:")
+old_word=input("Enter the word to be removed:")
+new_word=input("Enter the new word:")
+
+result=ch.replace(old_word,new_word)
+
+print("updated string is:",result)
