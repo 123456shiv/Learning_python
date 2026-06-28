74a19e97fb914d5b6320ac346cd49f68b81e35dd
Shiv Sharan Tripathi <tripathishivsharan898@gmail.com>
2026-06-28 22:34:31 +0530

    substring in string

---
+++ substring.py
@@ -0,0 +1,8 @@
+string=input("Enter the main string:")
+substring=input("Enter a substring:")
+
+if string.find(substring)!=-1:
+    print("Substring found")
+
+else:
+    print("Substring not found")
