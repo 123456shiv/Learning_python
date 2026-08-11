baa3645aeb216e1f6734f010fb7fd015d2936a9c
Shiv Sharan Tripathi <tripathishivsharan898@gmail.com>
2026-08-11 16:09:12 +0530

    create a list and remove the objects

---
+++ listfunction.py
@@ -0,0 +1,9 @@
+def list_function():
+    my_list = ['apple', 'banana', 'cherry', 'date', 'elderberry']
+
+    my_list.append('fig')
+    my_list.remove('banana')
+
+    print("List after appending 'fig':", my_list)
+
+list_function()
