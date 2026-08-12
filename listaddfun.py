8b46820124fa61c2cd1af26f393342f283ac26f6
Shiv Sharan Tripathi <tripathishivsharan898@gmail.com>
2026-08-12 23:22:00 +0530

    create a list and add the objects

---
+++ listaddfun.py
@@ -0,0 +1,7 @@
+def list_function():
+    my_list = ['apple', 'banana', 'cherry', 'date', 'elderberry']
+
+    my_list.append('watermelon')
+    print("List after appending 'watermelon':", my_list)
+
+list_function()
