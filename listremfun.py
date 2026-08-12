bdd505e9dc64f537147f39d1060a1ce71cffa7f0
Shiv Sharan Tripathi <tripathishivsharan898@gmail.com>
2026-08-12 23:24:11 +0530

    create a list and remove the element

---
+++ listremfun.py
@@ -0,0 +1,7 @@
+def list_function():
+    my_list = ['apple', 'banana', 'cherry', 'date', 'elderberry']
+
+    my_list.remove('banana')
+    print("List after removing 'banana':", my_list)
+
+list_function()
